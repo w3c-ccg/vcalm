a8ee5d8db2c959f8f841ba2babb80cc6a443142f
Mahmoud Alkhraishi <mkhraisha@hotmail.com>
2021-03-11 15:38:58 -0500

    release prep

---
+++ packages/plugfest-2021/config/SecureKey.js
@@ -0,0 +1,32 @@
+module.exports = {
+    name: "SecureKey",
+    issueCredentialConfiguration: [
+        {
+            id: "did:trustbloc:testnet.trustbloc.dev:EiAFEDjO4aF0ItSmvK1ehKwtqyZlXExK8l70D6EP-TuF9A",
+            endpoint: "https://issuer.sandbox.trustbloc.dev/vc-issuer-interop/credentials/issueCredential",
+            proofType: "Ed25519Signature2018",
+            options: {
+                assertionMethod: "did:trustbloc:testnet.trustbloc.dev:EiAFEDjO4aF0ItSmvK1ehKwtqyZlXExK8l70D6EP-TuF9A#cAl-0D3UEU2WOSlDrQXQWG2YQ51QIL6Ho8aeXaVzkfw"
+            }
+        },
+        {
+            id: "did:key:z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd",
+            endpoint: "https://issuer.sandbox.trustbloc.dev/vc-issuer-interop/credentials/issueCredential",
+            proofType: "Ed25519Signature2018",
+            options: {
+                assertionMethod: "did:key:z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd#z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd"
+            }
+        }
+    ],
+    verifyCredentialConfiguration: {
+        endpoint: "https://verifier.sandbox.trustbloc.dev/verifier1/verifier/credentials",
+        didMethodsSupported: [ "did:key:", "did:web:" ],
+        linkedDataProofSuitesSupported: [ "Ed25519Signature2018" ]
+    },
+    verifyPresentationConfiguration: {
+        endpoint: "https://verifier.sandbox.trustbloc.dev/verifier1/verifier/presentations"
+    },
+    credentials: require('../__fixtures__/credentials'),
+    verifiableCredentials: require('../__fixtures__/verifiableCredentials'),
+    verifiablePresentations: require('../__fixtures__/verifiablePresentations')
+};
