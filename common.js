eeb84358b6b72fd6719cb119f225e283f8abb050
David I. Lehn <dlehn@digitalbazaar.com>
2021-09-19 14:53:26 -0600

    Implement initial OAS summary generation for ReSpec.

--- common.js
+++ common.js
@@ -314,3 +314,69 @@ function unComment(doc, content) {
     .replace(/-\s*- >/g, '-->')
     .replace(/-\s*-\s*&gt;/g, '--&gt;');
 }
+
+/********************* OAS Experiment *************************/
+function getEndpoint({apis, path}) {
+  let endpoint = {
+    post: {
+      summary: 'Error: API Endpoint not defined - ' + path
+    }
+  };
+
+  for(const api of apis) {
+    if(api.paths[path] !== undefined) {
+      endpoint = api.paths[path];
+    }
+  }
+
+  return endpoint;
+}
+
+function buildApiSummaryTables({config, document, apis}) {
+  const apiTables = document.querySelectorAll("table.api-summary-table");
+
+  // process every table
+  for(const table of apiTables) {
+    // set up the API summary table headers
+    const tableHeader = document.createElement('tr');
+    tableHeader.innerHTML = '<th>Endpoint</th><th>Description</th>';
+    table.appendChild(tableHeader);
+
+    // summarize each API endpoint
+    for(const path of table.dataset.apiPath.split(/\s+/)) {
+      if(path.trim().length > 0) {
+        const endpoint = getEndpoint({apis, path});
+        console.log("ENDPOINT", endpoint);
+        for(const verb in endpoint) {
+          const {summary} = endpoint[verb];
+          const tableRow = document.createElement('tr');
+          tableRow.innerHTML =
+            `<td>${verb.toUpperCase()}&nbsp;${path}</td><td>${summary}</td>`;
+          table.appendChild(tableRow);
+        }
+      }
+    }
+    //const paths = (table.dataset.apiPath).split(' ');
+    //console.log("PATHS", paths);
+  }
+}
+
+async function injectOas(config, document) {
+  try {
+    let issuerApi = await SwaggerParser.validate('issuer.yml');
+    console.log('API name: %s, Version: %s',
+      issuerApi.info.title, issuerApi.info.version);
+    let verifierApi = await SwaggerParser.validate('verifier.yml');
+    console.log('API name: %s, Version: %s',
+      verifierApi.info.title, verifierApi.info.version);
+    let holderApi = await SwaggerParser.validate('holder.yml');
+    console.log('API name: %s, Version: %s',
+      holderApi.info.title, holderApi.info.version);
+    const apis = [issuerApi, verifierApi, holderApi];
+
+    buildApiSummaryTables({config, document, apis});
+  }
+  catch(err) {
+    console.error(err);
+  }
+}
