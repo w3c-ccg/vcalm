bd332c02b11926953a2a3a3d58f9eef9dcc93285
eric-schuh <eric.schuh@gmail.com>
2024-05-14 11:48:28 -0700

    Update respec-oas.js

--- respec-oas.js
+++ respec-oas.js
@@ -31,7 +31,7 @@ function buildComponentTables({config, document, apis}) {
         for(const verb in endpoint) {
           const {servers} = endpoint[verb];
           const tableRow = document.createElement('tr');
-          const expectedCaller = ${servers[0].url}
+          const expectedCaller = ${servers[0].url};
           tableRow.innerHTML =
             `<td>${verb.toUpperCase()}&nbsp;${path}</td><td>${servers[0].url}</td>`;
           table.appendChild(tableRow);
